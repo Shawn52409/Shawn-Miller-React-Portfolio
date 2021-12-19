1203457af19a102d75dab319faedff46538bc902
Shawn Miller <shawnsmm@gmail.com>
2021-12-19 18:43:51 -0500

    Initial commit

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <Header />
+      <AboutMe />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
