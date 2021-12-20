dfb5b136bab908d81ef42aae11a888ae44512e30
Shawn Miller <shawnsmm@gmail.com>
2021-12-19 23:04:45 -0500

    imported components into app.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+// Import global styles
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+
+// Import components
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+// Import list of projects
+import projects from './projects';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
       <Header />
-      <AboutMe />
-      <Projects />
+      <About />
+      <Projects projects={projects} />
       <Contact />
     </div>
   );
