5bd67f5351f414cb76d5613b06b8644f546d8092
Shawn Miller <shawnsmm@gmail.com>
2021-12-27 17:15:40 -0500

    added routes to different navlinks in portfolio

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,28 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 // Import global styles
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 // Import components
-import Navbar from './components/Navbar';
-import Header from './components/Header';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
 import About from './components/About';
-import Projects from './components/Projects';
+import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 // Import list of projects
-import projects from './projects';
+// import projects from './components/projects';
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-      <Header />
-      <About />
-      <Projects projects={projects} />
-      <Contact />
-    </div>
+    <Router>
+    <NavBar />
+        <Route component={Home} path='/' exact />
+        <Route component={About} path='/about' />
+        <Route component={Portfolio} path='/portfolio' />
+        <Route component={Contact} path='/contact' />
+        <Route component={Resume} path='/resume' />
+    </Router>
   );
 }
 
