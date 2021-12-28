791b1d5daf43a038ed2df223b705d5355be4052a
Shawn Miller <shawnsmm@gmail.com>
2021-12-28 18:27:33 -0500

    added footer

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -6,25 +6,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 // Import components
-import Home from './components/Home';
+// import Home from './components/Home';
 import NavBar from './components/NavBar';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
+import Footer from './components/Footer';
 
-// Import list of projects
-// import projects from './components/projects';
 
 function App() {
   return (
     <Router>
     <NavBar />
-        <Route component={Home} path='/' exact />
-        <Route component={About} path='/about' />
+        {/* <Route component={Home} path='/' exact /> */}
+        <Route component={About} path='/' exact />
         <Route component={Portfolio} path='/portfolio' />
         <Route component={Contact} path='/contact' />
         <Route component={Resume} path='/resume' />
+    <Footer />
     </Router>
   );
 }
--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
 export default function Footer() {
     return(
-        <div>Footer!
-            
+        <div className='footer'>
+            <Row>
+                <Col md='4'>
+                    <p>Designed and Developed by Shawn Miller</p>
+                </Col>
+                <Col md='4'>
+                    <p>2021</p>           
+                </Col>
+                <Col md='4'>
+                    <ul className='iconLinks'>
+                        <li className='iconLinks'>
+                            <a href="https://github.com/Shawn52409" target='_blank' style={{ color: 'white' }}><AiFillGithub /></a>
+                        </li>
+                        <li className='iconLinks'>
+                            <a href="https://www.linkedin.com/in/shawn-miller-b44a36217/" target='_blank' style={{ color: 'white' }}><FaLinkedinIn /></a>
+                        </li>
+                        <li className='iconLinks'>
+                            <a href="https://twitter.com/ShawnMiller524" target='_blank' style={{ color: 'white' }}><AiOutlineTwitter /></a>
+                        </li>
+                    </ul>
+                </Col>
+            </Row>
         </div>
     );
 };
