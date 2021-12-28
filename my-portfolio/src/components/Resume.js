c3e4d94783f1917f4a19c4a4899a962d7fa6a0ae
Shawn Miller <shawnsmm@gmail.com>
2021-12-28 09:30:41 -0500

    imported react-pdf and displayed resume pdf

--- my-portfolio/src/components/Resume.js
+++ my-portfolio/src/components/Resume.js
@@ -1,9 +1,34 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+
 
 export default function Resume() {
+    
+    pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+    const [numPages, setNumPages] = useState(2);
+    const [pageNumber, setPageNumber] = useState(1);
+  
+    function onDocumentLoadSuccess({ numPages }) {
+      setNumPages(numPages);
+    }
+    
+    
     return(
-        <div>Resume!
-            
+        <div className='m-5 d-inline-flex'>
+            <Document 
+                file='/img/Miller-Shawn-Resume.pdf'
+                onLoadSucess={onDocumentLoadSuccess}
+                >
+                <Page pageNumber={1} />
+            </Document>
+
+            <Document 
+                file='/img/Miller-Shawn-Resume.pdf'
+                onLoadSucess={onDocumentLoadSuccess}
+            >
+                <Page pageNumber={2} />
+            </Document>
         </div>
     );
 };
