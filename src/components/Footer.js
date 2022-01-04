cc14e00185404271ea95dd5f3af98a7310c20484
Shawn Miller <shawnsmm@gmail.com>
2022-01-03 23:14:24 -0500

    Removed imports not used

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { AiFillGithub, AiOutlineFontSize, AiOutlineTwitter } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
