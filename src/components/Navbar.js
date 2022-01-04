7d8f7fe1257373c77dc22209154a204fe84ee7a1
Shawn Miller <shawnsmm@gmail.com>
2022-01-03 23:13:23 -0500

    Made changes to navbar to more mobile friendly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { Navbar } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
     return(
         <div className='navBar' >
-            <Navbar>
-                <h1 className='myName p-3'>Shawn Miller</h1>
-                    <NavLink to='/' exact className='link p-3'>
-                        About Me
-                    </NavLink> 
-                    <NavLink to='/portfolio' className='link p-3'>
-                        Portfolio
-                    </NavLink>                   
-                    <NavLink to='/contact' className='link p-3'>
-                        Contact
-                    </NavLink>                   
-                    <NavLink to='/resume' className='link p-3'>
-                        Resume
-                    </NavLink>                   
-            </Navbar>   
+            <Row>
+                <Col xs lg='6'>
+                    <h1 className='myName p-3'>Shawn Miller</h1>
+                </Col>
+                <Col md='auto'>
+                    <NavLink to='/' exact className='link p-3 text-center'>About Me</NavLink> 
+                </Col>    
+                <Col md='auto'>
+                    <NavLink to='/portfolio' className='link p-3 text-center'>Portfolio</NavLink>                   
+                </Col>    
+                <Col md='auto'>
+                    <NavLink to='/contact' className='link p-3 text-center'>Contact</NavLink>                   
+                </Col>    
+                <Col md='auto'>
+                    <NavLink to='/resume' className='link p-3 text-center'>Resume</NavLink>
+                </Col>    
+            </Row>                   
         </div>
     );
 };
