3eca79fd659eee98f7ac21a1fa41dbe014308bde
Shawn Miller <shawnsmm@gmail.com>
2022-01-31 23:19:48 -0500

    Updated styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,19 +7,19 @@ export default function NavBar() {
     return(
         <div className='navBar' >
             <Row>
-                <Col xs lg='6'>
-                    <h1 className='myName p-3'>Shawn Miller</h1>
+                <Col xs lg='4'>
+                    <h1 className='myName p-3 text-center'>Shawn Miller</h1>
                 </Col>
-                <Col md='auto'>
+                <Col md='auto' className='text-center'>
                     <NavLink to='/' exact className='link p-3 text-center'>About Me</NavLink> 
                 </Col>    
-                <Col md='auto'>
+                <Col md='auto' className='text-center'>
                     <NavLink to='/portfolio' className='link p-3 text-center'>Portfolio</NavLink>                   
                 </Col>    
-                <Col md='auto'>
+                <Col md='auto' className='text-center'>
                     <NavLink to='/contact' className='link p-3 text-center'>Contact</NavLink>                   
                 </Col>    
-                <Col md='auto'>
+                <Col md='auto' className='text-center'>
                     <NavLink to='/resume' className='link p-3 text-center'>Resume</NavLink>
                 </Col>    
             </Row>                   
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -18,49 +18,49 @@ export default function Portfolio() {
 
     return(
         <div className='row justify-content-evenly' style={{ backgroundImage: `url(${backgroundImage})` }}>
-            <article className="project1" className="container-md border col-md-3 p-3 m-3">
+            <article className="project1" className="container-md col-md-3 p-3 m-3">
                 <h3 className="text-center p-2">{projects[0].name}</h3>                
                 <a href={projects[0].deployedLink} target="_blank"><img className='projectImg' src={powerUp}/></a>                
                 <a href={projects[0].repoLink} target="_blank" className='githubLink'><br/><AiFillGithub size='25'/></a>
                 <div class="mt-2 border"></div>
                 <p>{projects[0].description}</p>
             </article>
-            <article className="project1" className="container-md border col-md-3 p-3 m-3">
+            <article className="project1" className="container-md col-md-3 p-3 m-3">
                 <h3 className="text-center p-2">{projects[1].name}</h3>                
                 <a href={projects[1].deployedLink} target="_blank"><img className='projectImg' src={musicReview}/></a>                
                 <a href={projects[1].repoLink} target="_blank" className='githubLink'><br/><AiFillGithub size='25'/></a>
                 <div class="mt-2 border"></div>
                 <p>{projects[1].description}</p>
             </article>
-            <article className="project2" className="border col-md-3 p-3 m-3">
+            <article className="project2" className="col-md-3 p-3 m-3">
                 <h3 className="text-center p-2">{projects[2].name}</h3>                
                 <a href={projects[2].deployedLink} target="_blank"><img className='projectImg' src={healthWellness}/></a>                
                 <a href={projects[2].repoLink} target="_blank" className='githubLink'><br/><AiFillGithub size='25' /></a>
                 <div class="mt-2 border"></div>
                 <p>{projects[2].description}</p>
             </article>
-            <article className="project3" className="border col-md-3 p-3 m-3">
+            <article className="project3" className="col-md-3 p-3 m-3">
                 <h3 className="text-center p-2">{projects[3].name}</h3>                
                 <a href={projects[3].deployedLink} target="_blank"><img className='projectImg' src={weatherDashboard}/></a>                
                 <a href={projects[3].repoLink} target="_blank" className='githubLink'><br/><AiFillGithub size='25' /></a>
                 <div class="mt-2 border"></div>
                 <p>{projects[3].description}</p>
             </article>
-            <article className="project4" className="border col-md-3 p-3 m-3">
+            <article className="project4" className="col-md-3 p-3 m-3">
                 <h3 className="text-center p-2">{projects[4].name}</h3>                
                 <a href={projects[4].deployedLink} target="_blank"><img className='projectImg' src={codingQuiz}/></a>                
                 <a href={projects[4].repoLink} target="_blank" className='githubLink'><br/><AiFillGithub size='25' /></a>
                 <div class="mt-2 border"></div>
                 <p>{projects[4].description}</p>
             </article>
-            <article className="project5" className="border col-md-3 p-3 m-3">
+            <article className="project5" className="col-md-3 p-3 m-3">
                 <h3 className="text-center p-2">{projects[5].name}</h3>                
                 <a href={projects[5].deployedLink} target="_blank"><img className='projectImg' src={budgetTracker}/></a>                
                 <a href={projects[5].repoLink} target="_blank" className='githubLink'><br/><AiFillGithub size='25' /></a>
                 <div class="mt-2 border"></div>
                 <p>{projects[5].description}</p>
             </article>
-            {/* <article className="project6" className="border col-md-3 p-3 m-3">
+            {/* <article className="project6" className="col-md-3 p-3 m-3">
                 <h3 className="text-center p-2">{projects[6].name}</h3>                
                 <a href={projects[6].deployedLink} target="_blank"><img className='projectImg' src={workoutTracker}/></a>                
                 <a href={projects[6].repoLink} target="_blank" className='githubLink'><br/><AiFillGithub size='25' /></a>
